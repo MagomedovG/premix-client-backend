53d3fc54b06ea3d01b7f3d4a97a9b874de8e3e58
MagomedovG <gamzat9953@gmail.com>
2026-03-30 01:50:00 +0300

    add env conf2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose.connect(
-    process.env.MONGO_URI || ""
+    process.env.MONGO_URL || ""
   );
 
   console.log("🟢 MongoDB Atlas connected");
